4c8c10f645d6c74036e64656e225c2710f3b1ac3
Harshil <harshil.parmar9087@gmail.com>
2021-05-22 22:56:59 +0530

    added without context IO flow

--- src/seq/flow/pattern/SequentialFlow.java
+++ src/seq/flow/pattern/SequentialFlow.java
@@ -11,6 +11,19 @@ public class SequentialFlow<I, O> implements ISequentialFlow<I, O> {
         this.current = current;
     }
 
+    /**
+     * @param next   This is the next Action to be performed.
+     * @param <NewO>
+     * @return
+     *
+     * This is the example of recursion. below code will point current to next and next will run current first
+     *
+     * if we need to remove context from this example then below code would become like...
+     * return new SequentialFlow(() -> {
+     *             current.run();
+     *             next.run();
+     *         });
+     */
     @Override
     public <NewO> ISequentialFlow<I, NewO> assembleFlow(ISequentialAction<O, NewO> next) {
         return new SequentialFlow<>(input -> next.run(current.run(input)));
